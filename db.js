f26bc18c367304c4f1b0fb77b70cc547342a87d5
chedi-debiche <ch.debiche@gmail.com>
2024-10-08 16:34:39 +0200

    connection fixed

--- db.js
+++ db.js
@@ -10,7 +10,11 @@ module.exports = () => {
   try {
      //mongoose.connect("mongodb://localhost:27017/fitmind", connectionParams);
       //  mongoose.connect(process.env.DBURL, connectionParams);
-      mongoose.connect('mongodb+srv://chedidebiche1:VaBy9i7gpiq4Mvfe@cluster0.azzzc.mongodb.net/', connectionParams);
+      mongoose.connect('mongodb+srv://chedidebiche1:VaBy9i7gpiq4Mvfe@cluster0.azzzc.mongodb.net/',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true,  // This enables TLS
+});
 
     console.log('Connected to database successfully');
   } catch (error) {
